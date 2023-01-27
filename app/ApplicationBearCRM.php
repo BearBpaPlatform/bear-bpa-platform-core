77b9486b664477c7930b2527b9a8884cae6fb06f
SidorkinAlex <palach.orl@mail.ru>
2023-01-27 18:39:11 +0300

    add App core file

---
+++ app/ApplicationBearCRM.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace SidorkinAlex\BearERP;
+
+
+use co;
+use DI\Container;
+use SidorkinAlex\BearERP\controllers\ControllerFactory;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+class ApplicationBearCRM
+{
+    protected static Container $container;
+    public function __construct()
+    {
+        self::$container = new Container();
+    }
+
+    public static function getDIController(): Container
+    {
+        return self::$container;
+    }
+
+    public function start(Request $request,Response $response){
+        $this->bootstrap();
+        self::getDIController()->set('Swoole\Http\Response',$response);
+        self::getDIController()->set('Swoole\Http\Request',$request);
+        $controller = self::getDIController()->call(['\SidorkinAlex\BearERP\controllers\ControllerFactory', 'buildController'],[$request]);
+        Utils\LogUtils::log("controller",$controller);
+        $method = self::getDIController()->call(['\SidorkinAlex\BearERP\controllers\ControllerFactory', 'getMethod'],[$request]);
+        $response = $controller->callActionMethod(self::getDIController(),$method);
+        $response->end("");
+    }
+
+    protected function bootstrap()
+    {
+    }
+
+}
